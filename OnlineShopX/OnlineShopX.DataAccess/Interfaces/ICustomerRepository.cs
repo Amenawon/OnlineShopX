8820e8b93282abea88e4016dfa176a40ce8eb214
Amenawon <menaesezobor@gmail.com>
2018-07-30 16:44:44 +0100

    Create the Interface repository class

---
+++ OnlineShopX/OnlineShopX.DataAccess/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineShop.Abstractions;
+
+namespace OnlineShopX.DataAccess.Interfaces
+{
+   public interface ICustomerRepository
+    {
+        IEnumerable<Customers> GetAllCustomersAsync();
+        Task<Customers> GetCustomerByIdAsync(int customerId);
+        Task<int> CreateCustomerAsync(Customers item);
+        void UpdateCustomerDetail(Customers item);
+        void DeleteCustomer(int customerId);
+
+    }
+}
