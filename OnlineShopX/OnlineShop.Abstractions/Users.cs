6b6086901e86c271ea0ae065cbb0770c96bb87fa
Mesezobor <mesezobor@eminenttechnology.com>
2018-08-14 19:32:04 +0100

    Add the User Table
    Add the User Table

---
+++ OnlineShopX/OnlineShop.Abstractions/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Abstractions
+{
+    public enum Gender
+    {
+        Female,
+        Male
+    }
+    public class Users
+    {
+     
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public int City { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
