8ddcdba447152c06b41cd3f3f018e71489e67aa4
Mena Esezobor <mesezobor@eminenttechnology.com>
2019-05-17 19:27:02 +0100

    update

--- OnlineShopX/OnlineShop.Abstractions/Customers.cs
+++ OnlineShopX/OnlineShop.Abstractions/Customers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace OnlineShop.Abstractions
 {
     public class Customers
     {
+        [Key]
         public int CustomerId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
